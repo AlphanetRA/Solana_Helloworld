3fa69e6e57ad5818a04005d0b1ea21863b33d510
AlphanetRA <alphanetra0321@gmail.com>
2021-02-03 15:33:38 +0900

    Include library

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,8 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
